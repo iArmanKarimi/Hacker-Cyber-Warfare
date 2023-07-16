34161b82b8019b33d75775247c2b17c0357dbc20
Arman Karimi <mrevid2@gmail.com>
2023-07-17 03:13:34 +0430

    set more specific names for classes (status bar)

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -7,9 +7,11 @@ export default class Terminal extends Component {
       <>
         <div id="console-header">Konsole v1.0</div>
         <div id="console">
-          <div className="status-container">
+          <div id="status-container">
             <span className="font-OCR-A">Status:</span>
-            <span className="status">Not Connected :: 127.0.0.1</span>
+            <span id="connection-status">Not Connected</span>
+            <span> :: </span>
+            <span id="connection-IP">127.0.0.1</span>
           </div>
           <div id="interface">
             <div id="prompt-line">
