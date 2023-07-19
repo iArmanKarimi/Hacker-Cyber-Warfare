2b07e7a46c69016f091dc479dd586bb4585efaa7
Arman Karimi <mrevid2@gmail.com>
2023-07-19 16:26:09 +0430

    Create Help.jsx

---
+++ src/Help.jsx
@@ -0,0 +1,100 @@
+/*
+
+@@ -0,0 +1,95 @@
+<h1>Hacker v3.1</h1>
+<i>Cyber Warfare</i>
+
+(Press 'Esc' to close)
+
+<b>Introduction:</b>
+Welcome to Hacker 3.1
+Thank you for downloading Hacker v3.1
+
+You have just been employed by [x]-K Corp. The undeground organization. From now on you work for [x]-K Corp. They have assigned you 12 missions. You will hack into networks for a living now.
+All the latest network software has been compiled into one application.
+Konsole v1.0. The 12 missions given are also to help evaluate the release of
+Konsole v1.0.  Happy Hacking.
+
+-[x]erver-Ki||ah 
+
+[x]-K Corp. 
+===============================================================
+<b>How to play:</b>
+Brief Instructions:
+1-DNS hostname, get i.p
+2-PING i.p
+3-Nmap scan for open ports
+4-Telnet connect to i.p 
+To hack any server, make sure you do the steps above in order. Once connected you carry out your missions
+
+
+Detailed Instructions:
+Before you do anything you need to login. Just type in any login name you desire.
+1-On the left side there are 12 numbers. Each one is a mission click on one to
+   get breifing for mission. You don't need to do them in logical order
+2-During mission breif you will get the hostname for the target. You need to
+    retrieve the i.p of the target. Click on the DNS button
+3-You should see this "DNS>" enter the hostname of the target
+    eg www.whatever.com and press enter
+4-After the i.p has been return you need to ping the network to see if the
+    connection is alive/online Click the PING button and enter the i.p
+    It may take a few times to get a reply so keep trying until you do.
+5-After you have a reply from the target. You need to scan the network for open
+    ports to connect to. Click the NMAP button and enter in the i.p of target
+6-When a port has been found you need to get a remote connection
+    click the TELNET button.
+7-When connected you need to login. User name is always root
+    for password you will need to use John Ripper.
+8-When connected carry out your mission.
+9-When you have done your mission. you need to remove all traces of yourself
+    you need to remove your logged i.p file.
+    your i.p is 192.168.0.1
+10-When logs have been removed you can close connection
+
+See below for commands.
+
+===============================================================
+<b>Commands:</b>
+quit- Will exit any of the programs below. Note: Can't use this command while the 
+program is running (scanning, ping, cracking etc.)
+exit- To close your remote connection (Telnet)
+ls- List all the Files & Folders in current directory (Things in yellow are directories)
+pwd- Show current directory
+cd (folder)- Change directory. eg. cd main      Use  cd ..   to go back a folder
+===============================================================
+<b>Tools & Commands:</b>
+DNS- Used to return the I.P of a hostname.
+
+PING- Used to determine if the server is alive.
+
+NMAP- Port Scanner to find open services.
+scan- scan an address for open ports: scan (i.p) eg. scan 127.0.0.1 
+
+TELNET- Telnet client. To connect to I.P
+open- open connection in telnet. use: open (i.p) (port) eg. open 127.0.0.1 23
+
+FILE COPYIER- No comment
+
+FILE DELETER- No comment
+
+VCK- Virus Creation Kit.
+
+JOHN RIPPER- Password Cracker. For Login 
+===============================================================
+<b>SECRETZ:</b>
+1- If you login as a particular nick you get 3 extra lives. (at the start of the game).
+2- There is a secret server. The address is right before your eyes :p.
+hint. the name of the server doesn't have www. Last letter in address is 't'.
+3- There is a password you can use so that you don't need to crack it each time.
+hint. here is the password in binary code :
+01101110-01101111-01110100-01101000-01101001-01101110-01100111 
+        ^ -------------------notice these to are the same----------------^
+here is the password in hex code :
+6E 6F 74 68 69 6E 67
+===============================================================
+<b>NOTE:</b>
+Some of the tools are real tools used in hacking but its not how you would use it in real life.(how you use it in the game is not how you use it in real life).
+===============================================================
+Copyright 2023 ?? Hacker v3.1-Cyber Warfare Web
+
+*/
