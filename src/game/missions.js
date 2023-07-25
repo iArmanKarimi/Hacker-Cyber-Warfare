c4e26ec861d7fef811cf7d22b4a2d1eb2f50d5d2
Arman Karimi <mrevid2@gmail.com>
2023-07-25 20:40:39 +0430

    convert .json to .js
    ECMA 6 doesn't have a package import support for json files (except for 'assert') so let's keep it this way.

--- src/game/missions.js
+++ src/game/missions.js
@@ -1,62 +1,62 @@
-[
+export default [
     {
         "target": "Microsoft Network",
-        "hostname": "www.microsoft.com",
+        "host": "www.microsoft.com",
         "details": "you need to hack into microsoft and get a copy of this file: 8320.dat. This file contains vulnerability information on windows."
     },
     {
         "target": "Telstra Communications",
-        "hostname": "www.telstra.com.au",
+        "host": "www.telstra.com.au",
         "details": "You need to hack into telstra and find and delete this file: acct57065. This will disable the internet and phone account of one of our enemies."
     },
     {
         "target": "Bankwest Australia",
-        "hostname": "www.bankwest.com.au",
+        "host": "www.bankwest.com.au",
         "details": "You need to hack into bankwest and find this file: sct:a121 get a copy of it then delete it. Rest of the files need to be corrupted, so send a virus."
     },
     {
         "target": "ICQ Network",
-        "hostname": "www.icq.com",
+        "host": "www.icq.com",
         "details": "You need to hack into icq. There has been some information posted which is false. So we need you to get a copy of the password. file: admin.pwl"
     },
     {
         "target": "New York Stock Exchange",
-        "hostname": "www.nystockexchange.com",
+        "host": "www.nystockexchange.com",
         "details": "You need to hack into nyse. We are evil bastards so we just felt like crashing the nyse network. Also corrupting files. send a virus to nyse."
     },
     {
         "target": "FBI Department",
-        "hostname": "www.fbi.gov.us",
+        "host": "www.fbi.gov.us",
         "details": "You need to hack into the FBI network. Our friend has a file in their database. You need to find the file and delete it. The file is: file012"
     },
     {
         "target": "Tafe WA College",
-        "hostname": "www.tafe.wa.edu",
+        "host": "www.tafe.wa.edu",
         "details": "You need to hack into tafe network. A student has been given a low mark. We need you to get a copy of the exam so we can change it. Teachers have been biased in marking so send a virus and corrupt all the files. The file to obtain is: 2.3535"
     },
     {
         "target": "CNet Network",
-        "hostname": "www.cnet.com",
+        "host": "www.cnet.com",
         "details": "You need to hack into CNet network. We need you to take the whole network down. Send a virus to them!"
     },
     {
         "target": "Trendmicro Antivirus",
-        "hostname": "www.trendmicro.com",
+        "host": "www.trendmicro.com",
         "details": "You need to hack into trendmicro. Trendmicro has got a copy of one of our worms. We need you to hack into the network and remove ALL traces of our worm source code. The source code file is: vi146"
     },
     {
         "target": "World Bank",
-        "hostname": "www.worldbank.com",
+        "host": "www.worldbank.com",
         "details": "You need to hack into worldbank. We suspect that 2 people have been stealing money from us. So we need you to hack into network and get a copy of these 2 bank accounts. The accounts are: asct: 78987 and asct: 45673"
     },
     {
         "target": "Macintosh Network",
-        "hostname": "www.apple.com",
+        "host": "www.apple.com",
         "details": "You need to hack into apple network. As you know we hate macintosh. One of our other employees has already hacked into the network. Unfortunately he left a file with some tracable information on it. We need you to delete the file and send a virus to ensure it doesn't trace back to us. The file is: delete_me.doc"
     },
     {
         "target": "eBay Network",
-        "hostname": "www.ebay.com",
+        "host": "www.ebay.com",
         "details": "You need to hack into ebay. Someone has hacked into ebay and changed a lot of prices. We are not sure which prices have been changed. Give em a virus in the DATA directory so they will have to add all the prices again."
     }
 ]
---
+++ src/game/servers.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        "host": "localhost",
+        "IP": "127.0.0.1",
+        "port": "69",
+        "port_name": "TFTP"
+    },
+    {
+        "host": "www.microsoft.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.telstra.com.au",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.bankwest.com.au",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.icq.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.nystockexchange.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.tafe.wa.edu",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.cnet.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.trendmicro.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.worldbank.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.apple.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    },
+    {
+        "host": "www.ebay.com",
+        "IP": "",
+        "port": "",
+        "port_name": ""
+    }
+]
