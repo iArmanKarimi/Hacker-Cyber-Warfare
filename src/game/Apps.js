96387b3eaf22a5d1ae78e208faa4a69813a2213f
Arman Karimi <mrevid2@gmail.com>
2023-07-26 01:08:32 +0430

    interface

---
+++ src/game/Apps.js
@@ -0,0 +1,16 @@
+export default class Apps {
+  // check server up
+  Ping() {}
+  // open ports
+  Nmap() {}
+  // host to IP
+  DNS() {}
+  // del file
+  Delete() {}
+  // copy file
+  Copy() {}
+  // Virus Creation Kit
+  VCK() {}
+  // connect to server
+  Telnet() {}
+}
