2f55b2ebc37ffade92370a3170a9e4c43c33ffa4
Arman Karimi <mrevid2@gmail.com>
2023-07-26 19:23:33 +0430

    random boolean

--- src/game/Random.js
+++ src/game/Random.js
@@ -1,21 +1,25 @@
 export default class Random {
-    static IP() {
-        return Array.from(
-            { length: 4 },
-            () => Math.floor(Math.random() * 255) + 1
-        ).join(".");
-    }
+  static Boolean() {
+    return Math.random() < 0.5;
+  }
 
-    static username() {
-        let users = ["root", "guest", "user", "admin"];
-        return users[Math.floor(Math.random() * users.length)];
-    }
+  static IP() {
+    return Array.from(
+      { length: 4 },
+      () => Math.floor(Math.random() * 255) + 1
+    ).join(".");
+  }
 
-    static logs(n = 8) {
-        const formatLogFile = () => [`${this.IP()}.log`, this.username()];
+  static username() {
+    let users = ["root", "guest", "user", "admin"];
+    return users[Math.floor(Math.random() * users.length)];
+  }
 
-        return Array.from({ length: n }, () => formatLogFile()).concat([
-            [`${this.IP()}.log`, "root"],
-        ]);
-    }
+  static logs(n = 8) {
+    const formatLogFile = () => [`${this.IP()}.log`, this.username()];
+
+    return Array.from({ length: n }, () => formatLogFile()).concat([
+      [`${this.IP()}.log`, "root"],
+    ]);
+  }
 }
