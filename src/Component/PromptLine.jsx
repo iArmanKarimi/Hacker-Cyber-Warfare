a6de3a4e0d2334bb2a8fbd17a9606a301fe94038
Arman Karimi <mrevid2@gmail.com>
2023-07-28 00:32:27 +0430

    change id names + function component

--- src/Component/PromptLine.jsx
+++ src/Component/PromptLine.jsx
@@ -1,13 +1,10 @@
-import { Component } from "react";
 import "./css/PromptLine.css";
 
-export default class PromptLine extends Component {
-  render() {
-    return (
-      <div id="prompt-line">
-        <span id="prompt-dir">Login:</span>
-        <input type="text" id="prompt" autoFocus />
-      </div>
-    );
-  }
+export default function PromptLine() {
+  return (
+    <div id="prompt-line">
+      <span id="prompt-line-prompt">Login:</span>
+      <input type="text" id="prompt-line-input" autoFocus />
+    </div>
+  );
 }
