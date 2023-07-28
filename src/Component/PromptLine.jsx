4dc835f9547218ccdaa58f2e9c6501f1ff5e8c4d
Arman Karimi <mrevid2@gmail.com>
2023-07-29 01:37:55 +0430

    login phase+intial login+run apps

--- src/Component/PromptLine.jsx
+++ src/Component/PromptLine.jsx
@@ -2,40 +2,85 @@ import "./css/PromptLine.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import bash from "./bash";
-import { appMessages, appNames } from "./run";
+import { appNames, run } from "./run";
 
 function PromptLine({ clearOutput, appendOutput }) {
+  const [username, setUsername] = useState('');
   const [promptInput, setPromptInput] = useState("");
   const [promptLabel, setPromptLabel] = useState("Login:");
+  const [askUsernameStep, setAskUsernameStep] = useState(true);
+  const [askPasswordStep, setAskPasswordStep] = useState(false);
+  const [initialLoginStep, setInitialLoginStep] = useState(true);
 
-  function handleCommandEnter(e) {
-    if (e.key === "Enter") {
-      // get arg for apps that require it
-      if (promptInput in appMessages) {
-        const appName = promptInput
-        const appMessage = appMessages[promptInput];
-        setPromptLabel(`${appName.toUpperCase()}>`);
-        appendOutput(appMessage);
-      }
-      // run app
-      if (promptInput in appNames) {
-        // TODO
-      }
-      else {
-        const command = promptInput
-        const result = bash(command);
-        if (result.path) {
-          setPromptLabel(result.path);
-        }
-        if (result.clearOutput) {
-          clearOutput();
-        }
-        if (result.output) {
-          appendOutput(result.output);
-        }
-      }
-      setPromptInput("");
+  // TODO
+  // function handlePasswordInput() {}
+  // function handleAppInput() {}
+  // function handleCommandInput() {}
+
+  function usernameInputStage() {
+    if (promptInput === 'root') {
+      appendOutput('Login: root')
+      setPromptLabel('Password:')
+      setAskPasswordStep(true)
+      setAskUsernameStep(false)
+    } else {
+      appendOutput('wrong username!')
     }
+  }
+
+  function initialLogin() {
+
+  }
+
+  function handleInputEnter(e) {
+    if (e.key !== "Enter") return
+    if (initialLoginStep) {
+      initialLogin()
+    }
+    if (askUsernameStep) {
+      usernameInputStage()
+    }
+    if (askPasswordStep) {
+      setPromptInput('')
+      // TODO 
+      // if user runs john from menu, show them password
+      // if password is correct, update prompt label with path
+      if ('correct password') {
+        // ...
+        setAskPasswordStep(false)
+      } else {
+        appendOutput("wrong password!")
+      }
+      return
+    }
+    // Apps
+    if (promptInput in appNames) {
+      const app = promptInput
+      const result = run(app)
+      if (result.output) {
+        appendOutput(result)
+      }
+      if (result.openedConnection) {
+        setPromptInput('')
+        setPromptLabel('Login:')
+        setAskUsernameStep(true)
+        return
+      }
+    } // Commands
+    else {
+      const command = promptInput
+      const result = bash(command);
+      if (result.path) {
+        setPromptLabel(result.path);
+      }
+      if (result.clearOutput) {
+        clearOutput();
+      }
+      if (result.output) {
+        appendOutput(result.output);
+      }
+    }
+    setPromptInput("");
   }
 
   return (
@@ -46,7 +91,7 @@ function PromptLine({ clearOutput, appendOutput }) {
         id="prompt-line-input"
         value={promptInput}
         onChange={(e) => setPromptInput(e.target.value)}
-        onKeyDown={handleCommandEnter}
+        onKeyDown={handleInputEnter}
         autoFocus
       />
     </div>
